e356185665e2192b40d4d3136d3dcecb68d3181c
EthanNguyen18 <105961827@student.swin.edu.au>
2026-03-13 20:02:56 +1100

    Cleanup: Removed Flask and kept only Streamlit/Pandas

--- data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
-
-file_path = "data/melbourne_ta_reviews.csv"
-data = load_data(file_path)
-print(data.head())
